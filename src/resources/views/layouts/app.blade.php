209f45655eacd56b5c211a17f02729672a566c2b
miho <tsukko.a3255@gmail.com>
2024-08-04 19:55:05 +0900

    viewsの作成

---
+++ src/resources/views/layouts/app.blade.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- reset.css ress -->
+    <link rel="stylesheet" href="https://unpkg.com/ress/dist/ress.min.css" />
+    <link rel="stylesheet" href="{{ asset('css/common.css') }}">
+    @yield('css')
+    <title>Attendance</title>
+</head>
+
+<body>
+    <header class="header">
+        <p class="header__logo">Atte</p>
+        <div class="header__link">
+            <form class="link__item" action="/" method="">@csrf<input class="link__button" type="submit" value="ホーム">
+            </form>
+            <form class="link__item" action="/attendance" method="">@csrf<input class="link__button" type="submit"
+                    value="日付一覧"></form>
+            <form class="link__item" action="/login" method="">@csrf<input class="link__button" type="submit"
+                    value="ログアウト"></form>
+        </div>
+    </header>
+    <main class="main">
+        @yield('content')
+    </main>
+    <footer class="footer">
+        <div class="footer__copyright">
+            <small>
+                Atte,inc.
+            </small>
+        </div>
+    </footer>
+</body>
+
+</html>
