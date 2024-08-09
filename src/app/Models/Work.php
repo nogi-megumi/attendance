d41748630fe579b2296741ef25cfc55dc5de84b3
miho <tsukko.a3255@gmail.com>
2024-08-10 02:29:41 +0900

    create:テーブル、モデル

---
+++ src/app/Models/Work.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Work extends Model
+{
+    use HasFactory;
+
+    protected $fillable=[
+        'user_id',
+        'work_start',
+        'work_end',
+    ];
+    public function works(){
+        return $this->hasMany('App\Models\Break');
+    }
+}
