94e3816ee71902230bff7fe818d33ddf3627783e
miho <tsukko.a3255@gmail.com>
2024-08-25 21:29:04 +0900

    日付別indexアクションの追加

--- src/app/Http/Controllers/AttendanceController.php
+++ src/app/Http/Controllers/AttendanceController.php
@@ -3,11 +3,51 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Work;
+use App\Models\BreakTime;
+use Carbon\Carbon;
 
 class AttendanceController extends Controller
 {
-    public function index(){
+    public function index(Request $request){
         // 日付別のデータ一覧
-        return view('date');
+        dd($request);
+        $date=Carbon::parse($request->date ?? now()->format('Y-m-d'));
+        $attendances=Work::whereDate('work_start',$date)->pagenate(5);
+
+        // 時間差分の処理
+        foreach ($attendances as $index => $attendance) {
+            dd($index);
+            $breaks=$attendance->breakTimes;
+            // 休憩時間の差分
+            foreach ($breaks as $break) {
+                $break_start=$break->break_start;
+                $start_dt=new Carbon($break_start);
+                $break_end=$break->break_end;
+                $end_dt=new Carbon($break_end);
+
+                $diff_break=$start_dt->diffInSeconds($end_dt);
+            }
+            // 勤務時間の差分
+            $work_start= new Carbon($attendance->work_start);
+            $work_end = new Carbon($attendance->work_end);
+            $diff_seconds=$work_start->diffInSeconds($work_end);
+            $diff_work=$diff_seconds - $diff_break;
+            // 秒単位から、H:i:sにする
+            $break_hours
+            = floor($diff_break / 3600);
+            $minutes = floor(($diff_break % 3600) / 60);
+            $seconds = $diff_break % 60;
+            $break_dt = Carbon::createFromTime($break_hours, $break_minutes, $break_seconds);
+
+            $work_hours = floor($diff_work / 3600);
+            $work_minutes = floor(($diff_work % 3600) / 60);
+            $work_seconds = $diff_work % 60;
+            $work_dt = Carbon::createFromTime($work_hours, $work_minutes, $work_seconds);
+
+            $attendances['index']->break_time=$break_dt->format('H:i:s');
+            $attendances['index']->work_time = $work_dt->format('H:i:s');
+        }
+        return view('date',compact('date','attendances'));
     }
 }
--- src/resources/views/date.blade.php
+++ src/resources/views/date.blade.php
@@ -7,13 +7,12 @@
 @section('content')
 <div class="content">
     <div class="date">
-        <a class="date--link" {{-- href="{{route('attendances.index', ['date' => $date->subDay()->format('Y-m-d')]) }}"
-            --}}>
+        <a class="date--link" href="{{route('attendances.index', ['date' => $date->subDay()->format('Y-m-d')]) }}">
             &lsaquo;</a>
-        <span class="date--present">2024-08-04{{-- $date->format('Y-m-d') --}}
+        <span class="date--present">{{$date}}
         </span>
-        <a class="date--link" {{-- href="{{ route('attendances.index', ['date' => $date->addDay()->format('Y-m-d')]) }}"
-            --}}>&rsaquo;</a>
+        <a class="date--link"
+            href="{{ route('attendances.index', ['date' => $date->addDay()->format('Y-m-d')]) }}">&rsaquo;</a>
     </div>
     <div class="date__table">
         <table class="date__table--inner">
@@ -24,18 +23,17 @@
                 <th>休憩時間</th>
                 <th>勤務時間</th>
             </tr>
-            <tr>{{-- @foreach ($collection as $item)
-                @endforeach --}}
-                <td>テスト太郎</td>
-                <td>10:00:00</td>
-                <td>20:00:00</td>
-                <td>00:30:00</td>
-                <td>09:30:00</td>
+            <tr>@foreach ($attendances as $attendance)
+                @endforeach
+                <td>{{$attendance->user->name}}</td>
+                <td>{{$attendance->work_start}}</td>
+                <td>{{$attendance->work_end}}</td>
+                <td>{{$attendance->break_time}}</td>
+                <td>{{$attendance->work_time}}</td>
             </tr>
         </table>
         <div>
-            {{--ペジネーション
-            $attendance->links('vendor.pagination.custom')--}}
+            {{$attendance->appends(request()->query())->links('vendor.pagination.custom')}}
         </div>
     </div>
 </div>
