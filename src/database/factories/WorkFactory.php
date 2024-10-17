74b3985cd9852dfe809508ad4f28d534cbc6c1e3
miho <tsukko.a3255@gmail.com>
2024-10-17 22:28:35 +0900

    .gitignoreを修正

--- src/database/factories/WorkFactory.php
+++ src/database/factories/WorkFactory.php
@@ -7,18 +7,13 @@ use App\Models\User;
 
 class WorkFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array
-     */
     public function definition()
     {
-        $dammyDate=$this->faker->dateTimeBetween('2024-08-24 09:00:00','2024-08-24 10:00:00');
+        $dammyDate = $this->faker->dateTimeBetween('2024-08-24 09:00:00', '2024-08-24 10:00:00');
         return [
-            'user_id'=>User::factory(),
-            'work_start'=>$dammyDate->format('Y-m-d H:i:s'),
-            'work_end'=>$dammyDate->modify('+6hours')->format('Y-m-d H:i:s'),
+            'user_id' => User::factory(),
+            'work_start' => $dammyDate->format('Y-m-d H:i:s'),
+            'work_end' => $dammyDate->modify('+6hours')->format('Y-m-d H:i:s'),
         ];
     }
 }
--- src/routes/web.php
+++ src/routes/web.php
@@ -7,17 +7,6 @@ use App\Http\Controllers\AttendanceController;
 use App\Http\Controllers\RegisterUserController;
 use App\Http\Controllers\AuthenticatedSessionController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::post('/register', [RegisterUserController::class, 'store']);
 Route::post('/login', [AuthenticatedSessionController::class, 'store']);
 
